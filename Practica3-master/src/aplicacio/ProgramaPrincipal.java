e4a2f277de301ecfa039ff1f3a08c223ef22904a
PereEscoda <105195578+PereEscoda@users.noreply.github.com>
2022-12-15 11:05:44 +0100

    Add files via upload

---
+++ Practica3-master/src/aplicacio/ProgramaPrincipal.java
@@ -0,0 +1,127 @@
+package aplicacio;
+
+import java.util.Scanner;
+
+import Exception.ValorNoOk;
+public class ProgramaPrincipal {
+    static Scanner teclat = new Scanner(System.in);
+
+    public static void main(String[] args){
+    boolean sortir=false;
+
+    int limit1= 1;
+    int limit2 =4;
+    
+    System.out.println("Hola que tal broski.. etc etc i el menu\n");
+    int menu = llegirEnter(limit1, limit2);1
+
+    while(!sortir) 
+    {
+        switch(menu)
+        {
+
+            case 1:             //Llistes
+                limit2=3;
+                System.out.println("Where u goin bih???");
+                Llistes(llegirEnter(limit1, limit2)); 
+            
+            case 2:             //intercanvis
+
+
+
+            case 3:             //usuari
+
+
+
+            case 4:            //sortir
+        }
+    }
+
+
+
+
+
+
+    }
+
+
+    private static void Llistes(int menu)
+    {
+        int limit1=1;
+        int limit2 = 3;
+        switch(menu)
+        {
+            case 1: 
+                System.out.println("Quina tipus de llista vols selecionar?");
+                TotesLlistes(llegirEnter(limit1, limit2));
+
+            case 2: 
+
+                //Serveis actius
+
+            case 3: 
+
+                //Bens disponibles
+
+        }
+    }
+
+    private static void TotesLlistes(int menu)
+    {
+
+        switch(menu) {
+
+            case 1: //intercanvis
+
+            case 2: //producte
+
+                System.out.println("FUNCIONA");
+            case 3: //serveis
+
+            case 4: //usuaris
+
+            case 5: //bens
+        }
+
+
+    }
+
+    /**
+     * funcio per tenir un delay, rep els segons per parametre
+    * @param s --> segons que volem de retard
+    */
+    public static void delay(int s) {						
+        try {
+        Thread.sleep(s * 1000);
+        } catch (InterruptedException ex) {
+        Thread.currentThread().interrupt();
+        }
+        }
+    
+
+    public static int llegirEnter(int vMin, int vMax){                //ampliar amb el rang del menu
+
+        boolean llegit= false;
+        int num=0;
+
+        while(!llegit){
+
+            try{
+                num=Integer.parseInt(teclat.nextLine());
+                llegit= true;
+                if (num<vMin) throw new ValorNoOk(num);
+                if (num>vMax) throw new ValorNoOk(num);
+            }catch(NumberFormatException e){
+                System.out.println("ERROR, POSA UN PUTO NUMERO IL MENDIGATO :)");
+            }catch(ValorNoOk e){
+                System.out.println("POSA UN NUMERO DEL MENU");
+                llegit=false;
+            }
+
+        
+        }
+
+
+        return num;
+    }
+    }
---
+++ Practica3-master/src/zcoses obsoletes/LlistaServeis.java
@@ -0,0 +1,65 @@
+
+//NO TÉ PITNA QUE S'UTILIZI 
+
+
+
+
+import Dades.Classes.Data;
+import Dades.Classes.Servei;
+
+public class LlistaServeis {
+    private Servei[] llista;
+    private int nElem;
+
+
+
+    public LlistaServeis(int mida){             //decidir si es servei o producte
+        llista = new Servei[mida];
+        nElem=0;
+    }
+
+    public void afegirServei(Servei s)
+    {
+        if(nElem<llista.length)
+        {
+            llista[nElem]= s.copia();
+            nElem++;
+        }
+    }
+    public void esborraS(String nom)
+    {
+        boolean erased= true;
+        int i = 0;
+
+        while(i<nElem && !erased)
+        {
+            if(llista[i].getNom().equals(nom))
+            {
+                for(int j = i; j<nElem-1; j++)
+                {
+                    llista[j]= llista[j+1];
+
+                }
+                nElem--;
+            }
+            else i++;
+        }
+    }
+
+    public LlistaServeis actius(Data actual)
+    {
+        LlistaServeis aux = new LlistaServeis(nElem);
+
+        for(int i=0; i<nElem; i++)
+        {
+            if(llista[i].actiu(actual)) 
+            {
+                aux.afegirServei(llista[i]);
+            }
+        }
+
+        return aux;
+    }
+}
+
+
